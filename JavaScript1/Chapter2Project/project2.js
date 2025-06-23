da81bca38af65f511b521752824241904d7fa23d
Caleob King <150308461+Caleob@users.noreply.github.com>
2025-06-22 22:38:25 -0500

    Added CSS and JS clock project from Wesbos

---
+++ JavaScript1/Chapter2Project/project2.js
@@ -0,0 +1,36 @@
+/*Written by:
+Caleob King
+
+Date: 6/14/25
+*/
+
+//general variables
+const myName = "Caleob King";
+const n1 = 3.14159;
+const n2 = 2.718;
+
+// HTML elements
+const para1 = document.getElementById("p1");
+
+// Change text content in paragraph 1
+para1.textContent = myName;
+
+// Change text content in paragraph 2
+const numberSum = n1 + n2;
+document.getElementById("p2").textContent = numberSum;
+
+// Change text content in paragraph 3
+const numberMult = n1*n2;
+document.getElementById("p3").textContent = numberMult;
+
+// Change text content in paragraph 4
+const myNameAddNum = myName + n1;
+document.getElementById("p4").textContent = myNameAddNum;
+
+// Change text content in paragraph 5
+const myNameMultNum = myName * n2;
+document.getElementById("p5").textContent = myNameMultNum;
+
+// Change text content in paragraph 6
+const ageCompare = 47 >= numberMult;
+document.getElementById("p6").textContent = ageCompare;
