d92dfa213083ae0d4fea184c9d61cdcb3914644f
Caleob <caleobking@gmail.com>
2025-06-29 13:57:46 -0500

    created basic website and logged variables and objects of different types

--- Chapter3Project/script.js
+++ Chapter3Project/script.js
@@ -1,1 +1,34 @@
+/* 
+Written by:
+    Caleob King
+    Date:
+    6/29/25
+
+
+A variable to store your full name
+A variable to store your desired annual salary
+A variable to store your veteran status (hint: either you are a veteran or you are not a veteran)
+An array to store the names of three of your friends.
+An array to store the value of the desired annual salary for your three friends
+A literal object to store the first name, last name, and desired annual salary of yet another friend
+*/
+
+const fullName = "Caleob Tomas King";
+const desiredSalary = 75000;
+const veteranStatus = false;
+const friends = ['Matt', 'Sam','Richard'];
+const friendsSalaries = [70000,12000,45000];
+const anotherFriend = {
+  firstName: "Dana",
+  lastName: "Patton",
+  desiredAnnualSalary: 60000
+};
+
+console.log(fullName);
+console.log(desiredSalary);
+console.log(veteranStatus);
+console.log(friends);
+console.log(friendsSalaries);
+console.log(anotherFriend);
+
 
